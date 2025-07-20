d931dd0f0670f868ef96b9bfab23a05063cacf24
fadelakbars <fadhelakbarsallang@gmail.com>
2025-07-20 21:24:51 +0800

    resource controller

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,44 +1,24 @@
 import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { request, response } from 'express';
 
+let person = [
+  { id: 1, name: 'Superman' },
+  { id: 2, name: 'Batman' },
+  { id: 3, name: 'Wonder Woman' },
+]
+
 @Controller('hero')
 export class HeroController {
-  @Get()
-  index() {
-    return 'Ini hero';
+  @Get('index')
+  index(@Res() response) {
+    response.json(person);
   }
 
-  // return object
-  @Get('object')
-  object() {
-    return {
-      nama: 'Fadhel',
-    };
-  }
+  @Post('create')
+  create(@Req() request, @Res({passthrough: true}) response) {
+    const {id, name} = request.body;
+    person.push({ id, name});
 
-  // pakai express response
-  @Get('expressresponse')
-  expressResponse(@Req() request: Request): string {
-    return 'Ini express response';
-  }
-
-  @Get('expressrequest')
-  expressRequest(@Res() response) {
-    return response.send('Ini express request');
-  }
-
-  // passthrough digunakan jika kita ingin mengembalikan response dari express sambil tetap mengembalikan response dari NestJS
-  @Get('passthrough')
-  passthrough(@Req() request: Request, @Res() response) {
-    response.send('Ini express response');
-    return 'Ini NestJS response';
-  }
-
-  //request object
-  @Post('requestbody')
-  requestBody(@Req() request, @Res({ passthrough: true }) response) {
-    return {
-      body : request.body,
-    }
+    return person;
   }
 }
